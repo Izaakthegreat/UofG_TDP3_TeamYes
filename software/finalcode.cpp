bc9f869f7acbe6c7f66a0908a920b2f728d1703e
Izaak Hollander <93389753+Izaakthegreat@users.noreply.github.com>
2024-03-21 14:33:30 +0000

    Update finalcode.cpp

--- software/finalcode.cpp
+++ software/finalcode.cpp
@@ -1,7 +1,8 @@
 #include "mbed.h"
 #include "TCS3472_I2C.h"
 #include <cstring>
-// Line Sensors
+
+// Init Line Sensors
 InterruptIn R3(PTA13);
 DigitalIn R2(PTD5);
 DigitalIn R1(PTD0);
@@ -21,6 +22,8 @@ DigitalOut Lbackward(PTA4);
 
 // Interrupt LED (For testing)
 // DigitalOut LED(PTD1);
+DigitalOut RedLED(PTD1);
+DigitalOut BlueLED(PTB18);
 
 // Colour Sensors
 TCS3472_I2C rgb_sensor_L(PTE0, PTE1);
@@ -32,6 +35,8 @@ DigitalOut Electromagnet(PTE31);
 // Bluetooth
 UnbufferedSerial bluetooth(PTE22,PTE23);
 
+// Motor control functions. Float for dutycycle corresponds to motor speed.
+// Passing a negative number in will cause motors to reverse.
 void controlLeft(float dutycycle) {
     if (dutycycle > 0){
         Lforward = 1;
@@ -62,11 +67,14 @@ void controlRight(float dutycycle) {
     }
 }    
 
+// Command to move both motors forward
 void goForward (float speed){
     controlLeft(speed);
     controlRight(speed);
     }
 
+// Commands to turn left by moving one side forward and one side in reverse.
+// Also passes in a wait time in microseconds to allow for tuning turn duration.
 void turnLeft (float speed, int time){
     controlLeft(-speed);
     controlRight(speed);
@@ -79,9 +87,12 @@ void turnRight(float speed, int time){
     wait_us(time);
     }
 
+// Interrupt case if outer sensors are hit. Used to account for sharper turns.
 void interruptLeft(){
     float speed = 0.7;
-    // LED = 1;
+    // Checks if one or both of the outermost sensors are triggered.
+    // If only the outermost is triggered, a softer turn is performed.
+    // If both are triggered then a very sharp turn is performed. This mostly happens on sharp corners.
     if (!L2 == 0){
     controlLeft(-speed);
     controlRight(-speed);
@@ -118,6 +129,7 @@ void interruptRight(){
     wait_us(550000);}
     }
 
+// Command for breaking.
 void stop(){
     PwmR = 0;
     PwmL = 0;
@@ -126,9 +138,9 @@ void stop(){
 void line(){
     if((!R2 == false)&&(!L2 == false)){goForward(0.16);};
 
-    if((!R2 == true)&&(!L2 == false)){turnRight(0.7,100000);} //if Right Sensor is Black and Left Sensor is White then it will call turn Right function  
+    if((!R2 == true)&&(!L2 == false)){turnRight(0.7,110000);} //if Right Sensor is Black and Left Sensor is White then it will call turn Right function  
 
-    if((!R2 == false)&&(!L2 == true)){turnLeft(0.7,100000);}  //if Right Sensor is White and Left Sensor is Black then it will call turn Left function
+    if((!R2 == false)&&(!L2 == true)){turnLeft(0.7,110000);}  //if Right Sensor is White and Left Sensor is Black then it will call turn Left function
 
     if((!R2 == true)&&(!L2 == true)){goForward(0.16);};   //if Right Sensor and Left Sensor are at White color then it will call forword function
 }
@@ -155,29 +167,53 @@ void SigCheck(){
 
 void BTcontrol(){
     bool exit = 0;
+    char last_dir;
     while(exit == 0){
         if (bluetooth.readable()) {
             bluetooth.read(&c,8);  // Read serial
 
             if (strcmp("w",c)==0){    // Forward half speed
-                controlRight(0.18);
-                controlLeft(0.18);
+                controlRight(0.15);
+                controlLeft(0.15);
+                last_dir = 'F';
             }
             if ( strcmp("s",c) == 0 ){   // Reverse half speed
-                controlRight(-0.18);
-                controlLeft(-0.18);
+                controlRight(-0.15);
+                controlLeft(-0.15);
+                last_dir = 'B';
             }
-            if (strcmp ("a",c) == 0 ){  // Start turning left
-                controlRight(0.4);
-                controlLeft(-0.4);
+            if (strcmp ("d",c) == 0 ){  // Start turning left
+                controlRight(0.5);
+                controlLeft(-0.5);
+                last_dir = 'T';
             }
-            if (strcmp("d",c)==0){  // Start turning right
-                controlRight(-0.4);
-                controlLeft(0.4);
+            if (strcmp("a",c)==0){  // Start turning right
+                controlRight(-0.5);
+                controlLeft(0.5);
+                last_dir = 'T';
             }
             if (strcmp("b",c)==0){
-                controlRight(0);
-                controlLeft(0);
+                if(last_dir == 'F'){
+                    controlRight(-0.2);
+                    controlLeft(-0.2);
+                    wait_us(20000);
+                    controlRight(0);
+                    controlLeft(0);
+                    last_dir = 'N';
+                }
+                else if (last_dir == 'B'){
+                    controlRight(0.2);
+                    controlLeft(0.2);
+                    wait_us(20000);
+                    controlRight(0);
+                    controlLeft(0);
+                    last_dir = 'N';
+                }
+                else{
+                    controlRight(0);
+                    controlLeft(0);
+                    last_dir = 'N';
+                }
             }
             if (strcmp("p",c)==0){
                 Electromagnet = 0;
@@ -221,16 +257,31 @@ int main(){
     // Disk logic
     bool disk = 0;
     char disk_colour = 'N';
+    RedLED = 0;
+    BlueLED = 0;
     
     while(1){
+
+
         if (disk == 0){
-            disk_colour = 'N';
-        }
-        if (disk == 0||disk_colour == 'N'){
             Electromagnet = 1;  
+            disk_colour = 'N';
+            RedLED = 0;
+            BlueLED = 0;
+            // printf("NO DISK\n");
         }
         else if (disk == 1||disk_colour != 'N'){
             Electromagnet = 0;
+        }
+
+        if (disk_colour == 'R'){
+            RedLED = 1;
+            // printf("RED DISK\n");
+        }
+
+        if (disk_colour == 'B'){
+            BlueLED = 1;
+            // printf("BLUE DISK\n");
         }
         // Line following
         line();
@@ -253,13 +304,13 @@ int main(){
     // int RedRatR = static_cast<int>(red_ratio_L*10000);
     // printf('R= %d L= %d', RedRatR, RedRatL);
 
-    if ((red_ratio_L < thresholds_L[0])&&(red_ratio_R >= thresholds_R[0])){
+    if ((disk == 0)&&(red_ratio_L < thresholds_L[0])&&(red_ratio_R >= thresholds_R[0])){
         disk = 1;
         disk_colour='R';
         Electromagnet = 0;
 
     }
-    if((blue_ratio_L < thresholds_L[2])&&(blue_ratio_R >= thresholds_R[2])){
+    if((disk == 0)&&(blue_ratio_L < thresholds_L[2])&&(blue_ratio_R >= thresholds_R[2])){
         disk = 1;
         disk_colour='B';
         Electromagnet = 0;
@@ -268,14 +319,16 @@ int main(){
 
     if ((disk == true)&&(disk_colour=='R')&&(red_ratio_L >= thresholds_L[0])&&(red_ratio_R >= thresholds_R[0])){
         disk = 0;
-        disk_colour='N';
+        //disk_colour='N';
         Electromagnet = 1;
+        // printf("DROP OFF RED\n");
 
     }
     else if((disk == true)&&(disk_colour=='B')&&(blue_ratio_L >= thresholds_L[2])&&(blue_ratio_R >= thresholds_R[2])){
         disk = 0;
-        disk_colour='N';
+        //disk_colour='N';
         Electromagnet = 1;
+        // printf("DROP OFF BLUE\n");
     }
         wait_us(1000);
     }
